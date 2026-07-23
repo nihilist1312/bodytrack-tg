35af1d33dec445204f8ee94d70b9c0bc21ecf257
Dmitry <dmitry.lunev.09@icloud.com>
2026-07-24 00:17:42 +0300

    add user_id and date to BodyMetrics and make waterMass optional

--- include/models/body-metrics.hpp
+++ include/models/body-metrics.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <optional>
+#include <string>
 
 struct SegmentMass {
     double leftArm;
@@ -13,18 +14,22 @@ struct SegmentMass {
 
 // храним данные о метриках тела пользователя в абсолютных значениях, kg, cm
 struct BodyMetrics {
+    // id of the record in the database, not the user id
     int64_t id;
+    int64_t user_id;
+    std::string date; // format: YYYY-MM-DD, for example: 2026-07-23
 
-    // main
+    // necessary
     double weight;
     double height;
     uint8_t age;
     double muscleMass;
     double bodyFatMass;
-    double waterMass;
 
     // additional
+    std::optional<double> waterMass;
     std::optional<double> boneMass;
+    // value from 1 to 10, where 1 is the lowest and 10 is the highest
     std::optional<uint8_t> visceralFat;
     std::optional<double> proteinMass;
 
