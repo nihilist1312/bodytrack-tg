4746d2846f487fd40b74d5a57555a8ad7a61a799
Dmitry <dmitry.lunev.09@icloud.com>
2026-07-27 14:11:16 +0300

    fix message handler register

--- include/handlers/message-handler.hpp
+++ include/handlers/message-handler.hpp
@@ -1,21 +1,20 @@
 #pragma once
 
+#include "bot/message-service.hpp"
 #include "bot/user-session.hpp"
 #include "database/database.hpp"
-#include "message-loader.hpp"
-#include <cstddef>
+
 #include <tgbot/tgbot.h>
 #include <tgbot/types/Message.h>
 
-#include <unordered_map>
-#include <vector>
-
 class MessageHandler {
   public:
-    MessageHandler(TgBot::Bot& bot, Database& database, UserSessionManager& userSessionManager,
-                   MessageLoader& messageLoader)
-        : bot_(bot), database_(database), userSessionManager_(userSessionManager),
-          messageLoader_(messageLoader) {}
+    MessageHandler(TgBot::Bot& bot, Database& database,
+                   UserSessionManager& userSessionManager,
+                   MessageService& message_service)
+        : bot_(bot), database_(database),
+          userSessionManager_(userSessionManager),
+          message_service_(message_service) {}
 
     void handleMessage(const TgBot::Message::Ptr& message);
 
@@ -23,39 +22,28 @@ class MessageHandler {
     TgBot::Bot& bot_;
     Database& database_;
     UserSessionManager& userSessionManager_;
-    MessageLoader& messageLoader_;
+    MessageService& message_service_;
 
     void handleTextMessage(const TgBot::Message::Ptr& message);
     void handleFileMessage(const TgBot::Message::Ptr& message);
 
     // registration
-    void registerHandler(const TgBot::Message::Ptr& message);
-    void userNameHandler(const TgBot::Message::Ptr& message);
-    void userAgeHandler(const TgBot::Message::Ptr& message);
-    void userSexHandler(const TgBot::Message::Ptr& message);
+    void onName(const TgBot::Message::Ptr& message);
+    void onAge(const TgBot::Message::Ptr& message);
 
     // input metrics
-    void weightHandler(const TgBot::Message::Ptr& message);
-    void heightHandler(const TgBot::Message::Ptr& message);
-    void muscleMassHandler(const TgBot::Message::Ptr& message);
-    void bodyFatMassHandler(const TgBot::Message::Ptr& message);
+    void onWeight(const TgBot::Message::Ptr& message);
+    void onHeight(const TgBot::Message::Ptr& message);
+    void onMusculeMass(const TgBot::Message::Ptr& message);
+    void onFatMass(const TgBot::Message::Ptr& message);
 
     // input optional metrics
-    void waterMassHandler(const TgBot::Message::Ptr& message);
-    void boneMassHandler(const TgBot::Message::Ptr& message);
-    void visceralFatHandler(const TgBot::Message::Ptr& message);
-    void proteinMassHandler(const TgBot::Message::Ptr& message);
+    void onWaterMass(const TgBot::Message::Ptr& message);
+    void onBoneMass(const TgBot::Message::Ptr& message);
+    void onVisceralFat(const TgBot::Message::Ptr& message);
+    void onPriteinMass(const TgBot::Message::Ptr& message);
 
     // ввод в формате leftArm,rightArm,leftLeg,rightLeg,trunk (dev)
-    void muscleMassSegmentsHandler(const TgBot::Message::Ptr& message);
-    void fatMassSegmentsHandler(const TgBot::Message::Ptr& message);
-
-    // utils
-    // редактирует сообщение вставляя в текст строки из text_replace, а в кнопки из button_replace,
-    // где ключ это индекс кнопки
-    void editTextAndKeyboard(
-        const std::string& key, int64_t chat_id, int32_t id,
-        const std::vector<std::string>& text_replace = {},
-        const std::unordered_map<size_t, std::vector<std::string>>& button_replace = {});
-    void deleteMessage(const TgBot::Message::Ptr& message);
+    void onMuscleMassSegments(const TgBot::Message::Ptr& message);
+    void onFatMassSegments(const TgBot::Message::Ptr& message);
 };
--- src/handlers/message-handler.cpp
+++ src/handlers/message-handler.cpp
@@ -1,175 +1,69 @@
 #include "handlers/message-handler.hpp"
+
 #include "bot/user-session.hpp"
 #include "bot/user-states.hpp"
-#include "utils.hpp"
-#include <cstddef>
-#include <exception>
-#include <tgbot/types/InlineKeyboardMarkup.h>
 
-#include <memory>
+#include <string>
+
+#include <spdlog/spdlog.h>
+#include <tgbot/tgbot.h>
 #include <tgbot/types/Message.h>
-
 // dev
 void MessageHandler::handleFileMessage(const TgBot::Message::Ptr& message) {
-    UserSession& session = userSessionManager_.get_session(message);
+    UserSession& session = userSessionManager_.getSession(message);
 
-    deleteMessage(message);
+    message_service_.deleteMessage(message);
 }
 
 void MessageHandler::handleTextMessage(const TgBot::Message::Ptr& message) {
-    int64_t userId = message->from->id;
-    UserSession& session = userSessionManager_.get_session(message);
+    spdlog::debug("handle text message");
+    UserSession& session = userSessionManager_.getSession(message);
 
-    switch (session.currentState) {
-    case UserStates::RegisrationNeed:
-        registerHandler(message);
-        break;
+    switch (session.current_state) {
     case UserStates::InputUserName:
-        userNameHandler(message);
+        onName(message);
         break;
     case UserStates::InputUserAge:
-        userAgeHandler(message);
+        onAge(message);
         break;
-    case UserStates::InputUserSex:
-        userSexHandler(message);
-        break;
-    // case UserStates::InputWeight:
-    //     weightHandler(message);
-    //     break;
-    // case UserStates::InputHeight:
-    //     heightHandler(message);
-    //     break;
-    // case UserStates::InputMuscleMass:
-    //     muscleMassHandler(message);
-    //     break;
-    // case UserStates::InputBodyFatMass:
-    //     bodyFatMassHandler(message);
-    //     break;
-    // case UserStates::InputWaterMass:
-    //     waterMassHandler(message);
-    //     break;
-    // case UserStates::InputBoneMass:
-    //     boneMassHandler(message);
-    //     break;
-    // case UserStates::InputVisceralFat:
-    //     visceralFatHandler(message);
-    //     break;
-    // case UserStates::InputProteinMass:
-    //     proteinMassHandler(message);
-    //     break;
-    // case UserStates::InputMuscleMassSegments:
-    //     muscleMassSegmentsHandler(message);
-    //     break;
-    // case UserStates::InputFatMassSegments:
-    //     fatMassSegmentsHandler(message);
-    //     break;
     default:
-        // bot_.getApi().deleteMessage(message->chat->id, message->messageId);
+        message_service_.deleteMessage(message);
         break;
     }
 }
 
 void MessageHandler::handleMessage(const TgBot::Message::Ptr& message) {
-    std::cout << "msgHdl " << message->text << "\n";
-    UserSession& session = userSessionManager_.get_session(message);
-
-    if (!session.userData.has_value()) {
-        session.currentState = UserStates::RegisrationNeed;
-        session.userData = UserData{};
-    }
-
     if (message->document) {
         handleFileMessage(message);
-    } else if (!message->text.empty() && message->text[0] != '/') {
-        handleTextMessage(message);
-    }
-    // else {
-    //     deleteMessage(message);
-    // }
-}
-
-// игнорируем любой текст, ждем нажатия кнопки
-void MessageHandler::registerHandler(const TgBot::Message::Ptr& message) {
-    deleteMessage(message);
-}
-
-void MessageHandler::userNameHandler(const TgBot::Message::Ptr& message) {
-    static constexpr size_t MIN_NAME_SIZE = 2;
-    static constexpr size_t MAX_NAME_SIZE = 64;
-    deleteMessage(message);
-    auto& user_session = userSessionManager_.get_session(message);
-    auto name = normalize_name(message->text);
-
-    if (name.size() < MIN_NAME_SIZE || name.size() > MAX_NAME_SIZE) {
-        editTextAndKeyboard("invalid_name", user_session.chatId, user_session.lastMessageId, {},
-                            {{0, {message->from->firstName}}});
+    } else if (!message->text.empty()) {
+        if (message->text[0] != '/')
+            handleTextMessage(message);
     } else {
-        user_session.userData->user_name = name;
-        editTextAndKeyboard("request_age", user_session.chatId, user_session.lastMessageId);
-        user_session.currentState = UserStates::InputUserAge;
+        spdlog::debug("Unsupported message type. Delete");
+        message_service_.deleteMessage(message);
     }
 }
 
-void MessageHandler::editTextAndKeyboard(
-    const std::string& key, int64_t chat_id, int32_t id,
-    const std::vector<std::string>& text_replace,
-    const std::unordered_map<size_t, std::vector<std::string>>& button_replace) {
-    if (chat_id == 0 || id == 0) {
-        std::cout << "edit failed\n";
-    }
-    auto message_json = messageLoader_.getMessage(key);
-    std::string message_text = message_json["text"];
-    if (!text_replace.empty()) {
-        replace_by_vector(message_text, text_replace);
-    }
-    auto button_rows = message_json["buttons"];
-
-    size_t i = 0;
-    auto keyboard = std::make_shared<TgBot::InlineKeyboardMarkup>();
-    for (const auto& row : button_rows) {
-        std::vector<TgBot::InlineKeyboardButton::Ptr> buttons;
-
-        for (const auto& button : row) {
-            auto btn = std::make_shared<TgBot::InlineKeyboardButton>();
-
-            std::string text_button = button["text"];
-            auto it = button_replace.find(i);
-            if (it != button_replace.end()) {
-                replace_by_vector(text_button, it->second);
-            }
-            btn->text = text_button;
-            btn->callbackData = button["callback"];
-
-            buttons.push_back(btn);
-            ++i;
-        }
-
-        keyboard->inlineKeyboard.push_back(buttons);
-    }
-
-    bot_.getApi().editMessageText(message_text, chat_id, id, "", "", nullptr, keyboard);
-}
-
-void MessageHandler::userAgeHandler(const TgBot::Message::Ptr& message) {
-    static constexpr int MIN_AGE = 7;
-    static constexpr int MAX_AGE = 130;
-    deleteMessage(message);
-    auto& user_session = userSessionManager_.get_session(message);
-    int age = normalize_age(message->text);
-
-    if (MIN_AGE > age || age > MAX_AGE) {
-        editTextAndKeyboard("invalid_age", user_session.chatId, user_session.lastMessageId);
+void MessageHandler::onName(const TgBot::Message::Ptr& message) {
+    spdlog::debug("Name handle");
+    UserSession& session = userSessionManager_.getSession(message);
+    if (session.user_data->set_name(message->text)) {
+        spdlog::debug("Set user name: {}", session.user_data->user_name);
+        message_service_.editMessage(session, {"request_age"});
     } else {
-        user_session.userData->age = age;
-        editTextAndKeyboard("request_gender", user_session.chatId, user_session.lastMessageId);
-        user_session.currentState = UserStates::InputUserSex;
+        spdlog::debug("Incorrect name");
+        message_service_.editMessage(session, {"invalid_name"});
     }
 }
 
-void MessageHandler::userSexHandler(const TgBot::Message::Ptr& message) {
-    deleteMessage(message);
+void MessageHandler::onAge(const TgBot::Message::Ptr& message) {
+    spdlog::debug("Ade handle");
+    UserSession& session = userSessionManager_.getSession(message);
+    if (session.user_data->set_age(message->text)) {
+        spdlog::debug("Set user age: {}", session.user_data->age);
+        message_service_.editMessage(session, {"request_gender"});
+    } else {
+        spdlog::debug("Incorrect age");
+        message_service_.editMessage(session, {"invalid_age"});
+    }
 }
-
-void MessageHandler::deleteMessage(const TgBot::Message::Ptr& message) try {
-    bot_.getApi().deleteMessage(message->chat->id, message->messageId);
-} catch (const std::exception&) { std::cout << "delete failed\n"; }
