acd9930e3da84945e34d5be5a0498de946e74887
Dmitry <dmitry.lunev.09@icloud.com>
2026-07-27 13:10:26 +0300

    add database logging

--- src/database.cpp
+++ src/database.cpp
@@ -1,6 +1,10 @@
 #include "database/database.hpp"
+
 #include "database/sql-queries.hpp"
+
 #include <optional>
+
+#include <spdlog/spdlog.h>
 
 BodyMetrics getBodyMetricsFromRow(const SQLite::Statement& query) {
     BodyMetrics metrics;
@@ -20,7 +24,8 @@ BodyMetrics getBodyMetricsFromRow(const SQLite::Statement& query) {
         metrics.boneMass = query.getColumn("bone_mass").getDouble();
     }
     if (!query.getColumn("visceral_fat").isNull()) {
-        metrics.visceralFat = static_cast<uint8_t>(query.getColumn("visceral_fat").getInt());
+        metrics.visceralFat =
+            static_cast<uint8_t>(query.getColumn("visceral_fat").getInt());
     }
     if (!query.getColumn("protein_mass").isNull()) {
         metrics.proteinMass = query.getColumn("protein_mass").getDouble();
@@ -29,9 +34,11 @@ BodyMetrics getBodyMetricsFromRow(const SQLite::Statement& query) {
     if (!query.getColumn("muscle_left_arm").isNull()) {
         SegmentMass muscleSegments;
         muscleSegments.leftArm = query.getColumn("muscle_left_arm").getDouble();
-        muscleSegments.rightArm = query.getColumn("muscle_right_arm").getDouble();
+        muscleSegments.rightArm =
+            query.getColumn("muscle_right_arm").getDouble();
         muscleSegments.leftLeg = query.getColumn("muscle_left_leg").getDouble();
-        muscleSegments.rightLeg = query.getColumn("muscle_right_leg").getDouble();
+        muscleSegments.rightLeg =
+            query.getColumn("muscle_right_leg").getDouble();
         muscleSegments.trunk = query.getColumn("muscle_trunk").getDouble();
         metrics.muscleMassSegments = muscleSegments;
     }
@@ -58,7 +65,8 @@ UserData getUserDataFromRow(const SQLite::Statement& query) {
     return user_data;
 }
 
-void bindBodyMetricsToStatement(SQLite::Statement& query, const BodyMetrics& body_metrics) {
+void bindBodyMetricsToStatement(SQLite::Statement& query,
+                                const BodyMetrics& body_metrics) {
     query.bind(1, body_metrics.user_id);
     query.bind(2, body_metrics.date);
     query.bind(3, body_metrics.weight);
@@ -92,7 +100,8 @@ void bindBodyMetricsToStatement(SQLite::Statement& query, const BodyMetrics& bod
     }
 
     if (body_metrics.muscleMassSegments.has_value()) {
-        const SegmentMass& muscleSegments = body_metrics.muscleMassSegments.value();
+        const SegmentMass& muscleSegments =
+            body_metrics.muscleMassSegments.value();
         query.bind(12, muscleSegments.leftArm);
         query.bind(13, muscleSegments.rightArm);
         query.bind(14, muscleSegments.leftLeg);
@@ -120,38 +129,52 @@ void bindBodyMetricsToStatement(SQLite::Statement& query, const BodyMetrics& bod
 
 Database::Database(const std::string& db_path)
     : db(db_path, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE) {
+    spdlog::info("Open database: {}", db_path);
     // create users table if it doesn't exist
     db.exec(SQLQuery::createUsersTable);
+    if (db.getChanges() != 0) {
+        spdlog::info("Create User table");
+    }
 
     // create body_metrics table if it doesn't exist
     db.exec(SQLQuery::createBodyMetricsTable);
+    if (db.getChanges() != 0) {
+        spdlog::info("Create Metrics table");
+    }
 }
 
 // getters
 std::optional<BodyMetrics> Database::getLastBodyMetrics(int64_t user_id) const {
+    spdlog::debug("Database. Get user data for {}", user_id);
     SQLite::Statement query(db, SQLQuery::getLastBodyMetrics);
     query.bind(1, user_id);
 
     if (query.executeStep()) {
         return getBodyMetricsFromRow(query);
     } else {
+        spdlog::debug("Database. User {} not found", user_id);
         return std::nullopt;
     }
 }
 
 std::optional<BodyMetrics> Database::getBodyMetricsById(int64_t id) const {
+    spdlog::debug("Database. Get user metric by id {}", id);
     SQLite::Statement query(db, SQLQuery::getBodyMetricsById);
     query.bind(1, id);
 
     if (query.executeStep()) {
         return getBodyMetricsFromRow(query);
     } else {
+        spdlog::debug("Database. Metric {} not found", id);
         return std::nullopt;
     }
 }
 
-std::optional<BodyMetrics> Database::getBodyMetricsByUserIdAndDate(int64_t user_id,
-                                                                   const std::string& date) const {
+std::optional<BodyMetrics>
+Database::getBodyMetricsByUserIdAndDate(int64_t user_id,
+                                        const std::string& date) const {
+    spdlog::debug("Database. Get user metric by user ID {} and date {}",
+                  user_id, date);
     SQLite::Statement query(db, SQLQuery::getBodyMetricsByUserIdAndDate);
     query.bind(1, user_id);
     query.bind(2, date);
@@ -159,11 +182,15 @@ std::optional<BodyMetrics> Database::getBodyMetricsByUserIdAndDate(int64_t user_
     if (query.executeStep()) {
         return getBodyMetricsFromRow(query);
     } else {
+        spdlog::debug("Database. Metric not found. User {}, date {}", user_id,
+                      date);
         return std::nullopt;
     }
 }
 
-std::vector<BodyMetrics> Database::getBodyMetricsHistory(int64_t user_id) const {
+std::vector<BodyMetrics>
+Database::getBodyMetricsHistory(int64_t user_id) const {
+    spdlog::debug("Database. Get user metric history. User ID {}", user_id);
     SQLite::Statement query(db, SQLQuery::getBodyMetricsHistory);
     query.bind(1, user_id);
 
@@ -176,6 +203,7 @@ std::vector<BodyMetrics> Database::getBodyMetricsHistory(int64_t user_id) const 
 }
 
 std::optional<UserData> Database::getUserData(int64_t user_id) const {
+    spdlog::debug("Database. Get user data: {}", user_id);
     SQLite::Statement query(db, SQLQuery::getUserData);
     query.bind(1, user_id);
 
@@ -183,12 +211,14 @@ std::optional<UserData> Database::getUserData(int64_t user_id) const {
         UserData user_data = getUserDataFromRow(query);
         return user_data;
     } else {
+        spdlog::debug("Database. User not found {}", user_id);
         return std::nullopt;
     }
 }
 
 // modifiers
 bool Database::addUser(int64_t user_id, const UserData& user_data) {
+    spdlog::debug("Database. Add user {}", user_id);
     SQLite::Statement query(db, SQLQuery::addUser);
     query.bind(1, user_id);
     query.bind(2, user_data.user_name);
@@ -197,18 +227,30 @@ bool Database::addUser(int64_t user_id, const UserData& user_data) {
 
     query.exec();
 
-    return db.getChanges() == 1;
+    if (db.getChanges() == 0) {
+        spdlog::debug("Database. User {} already exist", user_id);
+        return false;
+    } else
+        return true;
 }
 
 bool Database::addBodyMetrics(const BodyMetrics& body_metrics) {
+    spdlog::debug("Database. Add metric with user {}, date {}",
+                  body_metrics.user_id, body_metrics.date);
     SQLite::Statement query(db, SQLQuery::addBodyMetrics);
     bindBodyMetricsToStatement(query, body_metrics);
     query.exec();
 
-    return db.getChanges() == 1;
+    if (db.getChanges() == 0) {
+        spdlog::debug("Database. Metric with user {}, date {} already exist",
+                      body_metrics.user_id, body_metrics.date);
+        return false;
+    } else
+        return true;
 }
 
 bool Database::editUserData(int64_t user_id, const UserData& user_data) {
+    spdlog::debug("Database. Edit user data. User ID {}", user_id);
     SQLite::Statement query(db, SQLQuery::editUserData);
     query.bind(1, user_data.user_name);
     query.bind(2, user_data.age);
@@ -216,47 +258,83 @@ bool Database::editUserData(int64_t user_id, const UserData& user_data) {
     query.bind(4, user_id);
     query.exec();
 
-    return db.getChanges() == 1;
+    if (db.getChanges() == 0) {
+        spdlog::debug("Database. User {} data not modifield", user_id);
+        return false;
+    } else
+        return true;
 }
 
-bool Database::editBodyMetricsById(int64_t id, const BodyMetrics& body_metrics) {
+bool Database::editBodyMetricsById(int64_t id,
+                                   const BodyMetrics& body_metrics) {
+    spdlog::debug("Database. Edit Metric by id {}", id);
     SQLite::Statement query(db, SQLQuery::editBodyMetricsById);
     bindBodyMetricsToStatement(query, body_metrics);
     query.bind(22, id); // Bind the ID for the WHERE clause
     query.exec();
 
-    return db.getChanges() == 1;
+    if (db.getChanges() == 0) {
+        spdlog::debug("Database. Metric {} not modifield", id);
+        return false;
+    } else
+        return true;
 }
 
-bool Database::editBodyMetricsByUserIdAndDate(int64_t user_id, const std::string& date,
+bool Database::editBodyMetricsByUserIdAndDate(int64_t user_id,
+                                              const std::string& date,
                                               const BodyMetrics& body_metrics) {
+    spdlog::debug("Database. Edit Metric by User ID {} and Date {}", user_id,
+                  date);
     SQLite::Statement query(db, SQLQuery::editBodyMetricsByUserIdAndDate);
     bindBodyMetricsToStatement(query, body_metrics);
     query.bind(22, user_id); // Bind the user_id for the WHERE clause
     query.bind(23, date);    // Bind the date for the WHERE clause
     query.exec();
 
-    return db.getChanges() == 1;
+    if (db.getChanges() == 0) {
+        spdlog::debug("Database. Metric with User {} and Date {} not modifield",
+                      user_id, date);
+        return false;
+    } else
+        return true;
 }
 
 bool Database::deleteBodyMetricsById(int64_t id) {
+    spdlog::debug("Database. Delete Metric by ID {}", id);
     SQLite::Statement query(db, SQLQuery::deleteBodyMetricsById);
     query.bind(1, id);
     query.exec();
-    return db.getChanges() == 1;
+    if (db.getChanges() == 0) {
+        spdlog::debug("Database. Metric {} does exist", id);
+        return false;
+    } else
+        return true;
 }
 
-bool Database::deleteBodyMetricsByUserIdAndDate(int64_t user_id, const std::string& date) {
+bool Database::deleteBodyMetricsByUserIdAndDate(int64_t user_id,
+                                                const std::string& date) {
+    spdlog::debug("Database. Delete Metriv by User ID {} and Date {}", user_id,
+                  date);
     SQLite::Statement query(db, SQLQuery::deleteBodyMetricsByUserIdAndDate);
     query.bind(1, user_id);
     query.bind(2, date);
     query.exec();
-    return db.getChanges() == 1;
+    if (db.getChanges() == 0) {
+        spdlog::debug("Database. Metric with User ID {} and Date {} does exist",
+                      user_id, date);
+        return false;
+    } else
+        return true;
 }
 
 bool Database::deleteUser(int64_t user_id) {
+    spdlog::debug("Database. Delete User {}", user_id);
     SQLite::Statement query(db, SQLQuery::deleteUser);
     query.bind(1, user_id);
     query.exec();
-    return db.getChanges() == 1;
+    if (db.getChanges() == 0) {
+        spdlog::debug("Database. User {} does exist", user_id);
+        return false;
+    } else
+        return true;
 }
