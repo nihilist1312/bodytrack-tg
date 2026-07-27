af4797973975eba94bb97d0daa320a5989a93e18
Dmitry <dmitry.lunev.09@icloud.com>
2026-07-27 12:45:19 +0300

    user session logging

--- src/bot/user-session.cpp
+++ src/bot/user-session.cpp
@@ -2,19 +2,25 @@
 
 #include <cstdint>
 
-#include <tgbot/types/Message.h>
+#include <spdlog/spdlog.h>
+#include <tgbot/tgbot.h>
 
 UserSession& UserSessionManager::getSession(int64_t user_id) {
+    spdlog::debug("Get session for user ID {}", user_id);
     auto [it, inserted] = sessions_.try_emplace(user_id);
     if (inserted) {
+        spdlog::debug("User session not found. Create");
         it->second.user_data = database_.getUserData(user_id);
+        spdlog::info("Active sessions: {}", sessions_.size());
     }
 
     return it->second;
 }
 
 void UserSessionManager::removeSession(int64_t user_id) {
+    spdlog::debug("Delete session user {}", user_id);
     sessions_.erase(user_id);
+    spdlog::info("Active sessions: {}", sessions_.size());
 }
 
 UserSession&
@@ -23,6 +29,7 @@ UserSessionManager::getSession(const TgBot::Message::Ptr& message) {
     UserSession& session = getSession(user_id);
 
     if (!session.user_data) {
+        spdlog::info("User {} is not exist. Registration", user_id);
         session.current_state = UserStates::RegistrationNeed;
         session.user_data.emplace();
         session.user_data->user_id = message->from->id;
