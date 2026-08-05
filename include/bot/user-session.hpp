10a8233a1da8057039bbcc44f8edb444afb40237
Dmitry <dmitry.lunev.09@icloud.com>
2026-08-05 18:53:26 +0300

    add commentary

--- include/bot/user-session.hpp
+++ include/bot/user-session.hpp
@@ -15,9 +15,27 @@
 
 struct UserSession {
     UserStates current_state = UserStates::MainMenu;
+
+    // пользовательские данные, инциализация при создании
     std::optional<UserData> user_data;
+
+    // Последнее измерение, для подсказок и меню
+    // Инциализация:
+    // 1. callback_handler: при выборе ручного режима (из database)
+    // 2. message_service: при открытии главного меню (из databse)
     std::optional<BodyMetrics> last_body_metrics;
+
+    // Промежуточный результат записи измерения.
+    // Инциализация:
+    // 1. message_service: при открытии главного меню {}
+    // 2. callback_handler: при выборе ручного режима создания записи {user_id, age}
     BodyMetrics body_metrics_draft;
+
+    // Промежуточный результат изменения сегментной массы
+    // Инициализация:
+    // 1. message_service: при открытии главного меню и меню выбора ДОП метрики {}
+    // 2. message_handler: при вводе данных {data} || {null}
+    // 3. callback_handler: при выборе сегментных мышц/жира {body_metrics_draft.segment_*}
     std::optional<SegmentMass> segment_mass_draft;
 
     int64_t chat_id = 0;
